c5615b6e05b0c1210a433b7b9184ddbe188b6244
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2013-02-19 20:22:21 +0100

    Liveness analysis!

---
+++ 1/Analysis.hs
@@ -0,0 +1,96 @@
+module Analysis
+--    ( liveness,
+--      Liveness()
+--    ) where
+where
+
+import Intermediate
+import qualified Data.Map as M
+import qualified Data.Array as A
+import qualified Data.Set as S
+
+data Liveness = Liveness [(Id, [(Instruction, S.Set Id, S.Set Id)])]
+
+pad :: a -> Int -> [a] -> [a]
+pad e n ls = ls ++ replicate (n - length ls) e
+
+instance Show Liveness where
+    show (Liveness fs) = unlines $ map formatFun fs
+        where formatFun (name, is) =
+                "== " ++ name ++ " ==\n" ++
+                line ++ "\n" ++
+                "| " ++ pad ' ' 40 "Instruction" ++ " | " ++ pad ' ' 20 "In" ++ " | " ++ pad ' ' 20 "Out" ++ " |\n" ++
+                line ++ "\n" ++
+                foldl (\a (i, ins, out) ->
+                    a ++ "| " ++
+                    pad ' ' 40 (show i) ++ " | " ++
+                    pad ' ' 20 (show $ S.toList ins) ++ " | " ++
+                    pad ' ' 20 (show $ S.toList out) ++ " | " ++ "\n"
+                ) "" is ++ line
+              line = pad '-' 90 ""
+
+liveness :: Program -> Liveness
+liveness = Liveness . map livenessFun
+
+livenessFun :: Function -> (Id, [(Instruction, S.Set Id, S.Set Id)])
+livenessFun (name, _, is) =
+    let n    = length is
+        ls   = labels is
+        suc  = arrayFromList $ map (succSet ls) (zip [0..] is)
+        gen  = arrayFromList $ map (S.fromList . genSet) is
+        kill = arrayFromList $ map (S.fromList . killSet) is
+        initial = [S.empty | _ <- [1..n]]
+        (ins, out) = livenessIter n suc gen kill initial initial
+    in (name, zip3 is ins out)
+
+livenessIter :: Int ->
+                A.Array Int [Int] ->
+                A.Array Int (S.Set Id) ->
+                A.Array Int (S.Set Id) ->
+                [S.Set Id] -> [S.Set Id] ->
+                ([S.Set Id], [S.Set Id])
+livenessIter n suc gen kill pins pout =
+    let pinsa = arrayFromList pins
+        pouta = arrayFromList pout
+        (ins, out) = foldr (\i (is, os)->
+                (S.union (gen A.! i) (S.difference (pouta A.! i) $ kill A.! i) : is,
+                 S.unions (map (pinsa A.!) $ suc A.! i) : os)
+            ) ([], []) [0 .. n-1]
+    in if   ins == pins && out == pout
+       then (ins, out)
+       else livenessIter n suc gen kill ins out
+
+arrayFromList :: [a] -> A.Array Int a
+arrayFromList ls = A.listArray (0, length ls - 1) ls
+
+genSet :: Instruction -> [Id]
+genSet (Set _ (AVal (Var y))) = [y]
+genSet (Set _ (UnOp _ (Var y))) = [y]
+genSet (Set _ (BinOp y _ (Var z))) = [y,z]
+genSet (Set _ (BinOp y _ _)) = [y]
+genSet (Set _ (Mem (Var y))) = [y]
+genSet (Store (Var x) y) = [x, y]
+genSet (Store _ y) = [y]
+genSet (If (x, _, Var y) _ _) = [x, y]
+genSet (If (x, _, _) _ _) = [x]
+genSet (Set _ (FCall _ args)) = args
+genSet (Return x) = [x]
+genSet _ = []
+
+killSet :: Instruction -> [Id]
+killSet (Set x _) = [x]
+killSet _ = []
+
+succSet :: M.Map String Int -> (Int, Instruction) -> [Int]
+succSet ls (_, Goto i) = [ls M.! i]
+succSet ls (_, If _ x y) = [ls M.! x, ls M.! y]
+succSet _ (_, Return _) = []
+succSet _ (i, _) = [i+1]
+
+labels :: [Instruction] -> M.Map Id Int
+labels = snd . foldl (\(i, m) e ->
+    (i+1,
+     case e of
+            Label l -> M.insert l i m
+            _       -> m)
+    ) (0, M.empty)
--- 1/Intermediate.hs
+++ 1/Intermediate.hs
@@ -1,23 +1,25 @@
 module Intermediate where
 
+import Data.List (intercalate)
+
 type Id = String
 type Arg = String
 data Atom = Var Id | IntVal Int
-    deriving (Eq, Show)
+    deriving Eq
 
 data UnOp = UMinus
-    deriving (Eq, Show)
+    deriving Eq
 data BinOp = Plus | Minus | Times
-    deriving (Eq, Show)
+    deriving Eq
 data RelOp = Less | Greater | Equal | LessEqual | GreaterEqual | NotEqual
-    deriving (Eq, Show)
+    deriving Eq
 
 data Exp = UnOp UnOp Atom
          | BinOp Id BinOp Atom
          | Mem Atom
          | AVal Atom
          | FCall Id [Arg]
-    deriving (Eq, Show)
+    deriving Eq
 
 type Cond = (Id, RelOp, Atom)
 
@@ -27,7 +29,43 @@ data Instruction = Label Id
                  | Goto Id
                  | If Cond Id Id
                  | Return Id
-    deriving (Eq, Show)
+    deriving Eq
 
 type Function = (Id, [Arg], [Instruction])
 type Program = [Function]
+
+instance Show UnOp where
+    show UMinus = "-"
+
+instance Show BinOp where
+    show Plus = "+"
+    show Minus = "-"
+    show Times = "*"
+
+instance Show RelOp where
+    show Less = "<"
+    show Greater = ">"
+    show Equal = "="
+    show LessEqual = "<="
+    show GreaterEqual = ">="
+    show NotEqual = "!="
+
+instance Show Exp where
+    show (UnOp uo a) = show uo ++ show a
+    show (BinOp i bo a) = i ++ " " ++ show bo ++ " " ++ show a
+    show (Mem a) = "M[" ++ show a ++ "]"
+    show (AVal a) = show a
+    show (FCall i as) = i ++ "(" ++ intercalate ", " (map show as) ++ ")"
+
+instance Show Instruction where
+    show (Label i) = "LABEL " ++ i
+    show (Set i e) = i ++ " := " ++ show e
+    show (Store a i) = "M[" ++ show a ++ "] := " ++ i
+    show (Goto i) = "GOTO " ++ i
+    show (If (i, ro, a) t f) = "IF " ++ i ++ " " ++ show ro ++ " " ++ show a ++
+                               " THEN " ++ t ++ " ELSE " ++ f
+    show (Return i) = "RETURN " ++ i
+
+instance Show Atom where
+    show (Var i) = i
+    show (IntVal i) = show i
